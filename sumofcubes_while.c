fd191975acc950dcffce8e3f61d0678385a855d5
Arvindraj <arvindultimate7352@gmail.com>
2017-01-25 18:23:33 +0530

    Add a Program and Configure Travis
    * Program to calculate sum of cubes of 'n' numbers
    * Add travis.yml file to Test-build latest changes and report the status

---
+++ sumofcubes_while.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+// Program to calculate the sum of cubes of 'n' numbers
+// 'n' - user Specified
+void main()
+{
+    int a=1,b,n,sc=0;
+    printf("\n\nEnter the number of inputs\n\n");
+    scanf("%d",&n);
+    while(a<=n)
+    {
+        printf("\n\nNumber %d\n",a);
+        scanf("%d",&b);
+        sc=sc+b*b*b;
+        a++;
+    }
+    printf("\n\nSum of cubes of these numbers is : %d",sc);
+}
