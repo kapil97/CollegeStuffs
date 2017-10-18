ef1e98c89b08679b5bb80d4725bdbfe695f421ee
kapil97 <kamleshwar.kamleshwar15@siesgst.ac.in>
2017-10-18 17:24:10 +0530

    test

--- prime.py
+++ prime.py
@@ -7,3 +7,15 @@ for i in range (2,100):
         if f==1:
             print i
             break;
+print "Enter number to check"
+n=int(raw_input())
+if n>1:
+    for i in range(2,n):
+        if(n%i)==0:
+            print(n,"isn't a prime number")
+            break
+    else:
+        print(n,"is prime")
+
+else:
+    print "enter number greater than 1"
